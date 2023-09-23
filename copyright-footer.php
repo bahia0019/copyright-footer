d9c3f701f0f0e818a80da1bae3e6f90b56ae9baa
William Bay <william@flauntyoursite.com>
2023-09-22 17:18:56 -0700

    Updated version number.

--- copyright-footer.php
+++ copyright-footer.php
@@ -4,7 +4,7 @@
  * Description:       Adds a footer with a Copyright date that updates every year automatically, with some additional options.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            William Bay
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
